cda59dc2fa8cb5858e371831041eb62f2bba7bf0
RodrigoAnciaes <81170574+RodrigoAnciaes@users.noreply.github.com>
2025-05-06 17:22:30 -0300

    Fix deprecated setup formatting

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
 from setuptools import setup, find_packages
 
-setup(name="nlp_gym",
-      version="0.1.0",
-      description="NLPGym - A toolkit for evaluating RL agents on Natural Language Processing Tasks",
-      author="Rajkumar Ramamurthy",
-      author_email="raj1514@gmail.com",
-      packages=find_packages(),
-      python_requires='>=3.7',
-      url="https://github.com/rajcscw/nlp-gym/",
-      install_requires=["numpy", "torch", "nltk", "flair", "pandas", "matplotlib",
-                        "seaborn", "scikit-learn", "tqdm", "edit_distance", "rich",
-                        "gym", "pytorch-nlp", "datasets", "wget"],
-      extras_require={'demo': ["tensorflow==1.15.0", "stable-baselines[mpi]"]},
-      tests_requires=["pytest"])
+setup(
+    name="nlp_gym",
+    version="0.1.0",
+    description="NLPGym - A toolkit for evaluating RL agents on Natural Language Processing Tasks",
+    author="Rajkumar Ramamurthy",
+    author_email="raj1514@gmail.com",
+    packages=find_packages(),
+    python_requires='>=3.7',
+    url="https://github.com/rajcscw/nlp-gym/",
+    install_requires=[
+        "numpy", "torch", "nltk", "flair", "pandas", "matplotlib",
+        "seaborn", "scikit-learn", "tqdm", "edit_distance", "rich",
+        "gym", "pytorch-nlp", "datasets", "wget"
+    ],
+    extras_require={
+        "demo": ["tensorflow", "stable-baselines3[extra,mpi]"],
+        "test": ["pytest"]
+    },
+)
